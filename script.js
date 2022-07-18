29a9f0a1909da0a022da189552394b7bd4b13e8b
Paardhiv Sarakam <88365669+Paardhiv10@users.noreply.github.com>
2022-07-18 07:23:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function buttonl(){
+    window.location.href="https://in.linkedin.com/in/paardhiv-sarakam-05b8aa204";
+}
+function buttong(){
+    window.location.href="https://github.com/Paardhiv10";
+}
+function buttont(){
+    window.location.href="https://twitter.com/PaardhivS";
+}
+function buttoni(){
+    window.location.href="https://www.instagram.com/paardhiv1/";
+}
+function project1(){
+    window.location.href="https://cubecoast.netlify.app/";
+}
+function project2(){
+    window.location.href="https://paardhiv-sarakam.netlify.app/";
+}
+function buttonr(){
+    window.location.href="https://drive.google.com/file/d/1rXhZDY9WlUR6alZ3AfYQ6aodGstJSjnE/view?usp=sharing";
+}
